04e815e57e97765420484ce4c5145a44c3880097
webxmsj <17625492424@163.com>
2019-05-21 11:54:49 +0800

    通讯录首字母索引基本实现

--- lib/home/contacts_page.dart
+++ lib/home/contacts_page.dart
@@ -42,6 +42,9 @@ class ContactsPage extends StatefulWidget {
 }
 
 class _ContactsPageState extends State<ContactsPage> {
+  Color _indexBarBg = Colors.transparent;
+  String _currentLetter = '';
+  ScrollController _scrollController;
   final ContactsPageData data = ContactsPageData.mock();
   final List<Contact> _contacts = [];
   final List<_ContactItem> _functionButtons = [
@@ -70,46 +73,111 @@ class _ContactsPageState extends State<ContactsPage> {
           print('添加公众号。');
         }),
   ];
+  final Map _letterPosMap = {INDEX_BAR_WORDS[0]: 0.0};
+  final List<Widget> _letters = INDEX_BAR_WORDS.map((String word) {
+    return Expanded(
+      child: Text(
+          word
+      ),
+    );
+  }).toList();
 
   @override
   void initState() {
     super.initState();
     _contacts..addAll(data.contacts)..addAll(data.contacts)..addAll(data.contacts);
     _contacts.sort((Contact a, Contact b) => a.nameIndex.compareTo(b.nameIndex));
+    _scrollController = new ScrollController();
+
+    // 计算用于 IndexBar 进行定位的关键通讯录列表项的位置
+    var _totalPos = _functionButtons.length * _ContactItem._height(false);
+    for (int i = 0; i < _contacts.length; i++) {
+      bool _hasGroupTitle = true;
+      if (i > 0 &&
+          _contacts[i].nameIndex.compareTo(_contacts[i - 1].nameIndex) == 0) {
+        _hasGroupTitle = false;
+      }
+      if (_hasGroupTitle) {
+        _letterPosMap[_contacts[i].nameIndex] = _totalPos;
+      }
+      _totalPos += _ContactItem._height(_hasGroupTitle);
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  String _getLetter(BuildContext context, double tileHeight, Offset globalPos) {
+    RenderBox _box = context.findRenderObject();
+    var local = _box.globalToLocal(globalPos);
+    int index = (local.dy ~/ tileHeight).clamp(0, INDEX_BAR_WORDS.length - 1);
+    return INDEX_BAR_WORDS[index];
+  }
+
+  Widget _buildIndexBar(BuildContext context, BoxConstraints constraints) {
+    final _totalHeight = constraints.biggest.height;
+    final double _tileHeight = _totalHeight / _letters.length; // 取整
+
+    return GestureDetector(
+      child: Column(
+        children: _letters,
+      ),
+      onVerticalDragDown: (DragDownDetails details) {
+        setState(() {
+          _indexBarBg = Colors.black26;
+        });
+        _currentLetter = _getLetter(context, _tileHeight, details.globalPosition);
+        _scrollController.animateTo(_letterPosMap[_currentLetter], curve: Curves.easeIn, duration: Duration(milliseconds: 200));
+      },
+      onVerticalDragEnd: (DragEndDetails details) {
+        setState(() {
+          _indexBarBg = Colors.transparent;
+        });
+        _currentLetter = '';
+      },
+      onVerticalDragCancel: () {
+        setState(() {
+          _indexBarBg = Colors.transparent;
+        });
+        _currentLetter = '';
+      },
+      onVerticalDragUpdate: (DragUpdateDetails details) {
+        setState(() {
+          _indexBarBg = Colors.black26;
+        });
+        _currentLetter = _getLetter(context, _tileHeight, details.globalPosition);
+        _scrollController.animateTo(_letterPosMap[_currentLetter], curve: Curves.easeIn, duration: Duration(milliseconds: 200));
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    
-    final List<Widget> _letters = INDEX_BAR_WORDS.map((String word) {
-      return Expanded(
-        child: Text(
-            word
-        ),
-      );
-    }).toList();
-    
     return Stack(
       children: <Widget>[
         Container(
           child: ListView.builder(
+              controller: _scrollController,
               itemBuilder: (BuildContext context, int index){
-            if (index < _functionButtons.length) {
-              return _functionButtons[index];
-            }
-            int _contactIndex = index - _functionButtons.length;
-            Contact _contact = _contacts[_contactIndex];
-            bool _isGroupTitle = true;
-            if (_contactIndex >= 1 && _contact.nameIndex == _contacts[_contactIndex - 1].nameIndex) {
-              _isGroupTitle = false;
-            }
-            return _ContactItem(
-              avatar: _contact.avatar,
-              title: _contact.name,
-              groupTitle: _isGroupTitle ? _contact.nameIndex : null,
-            );
-          },
-          itemCount: _contacts.length + 4,
+              if (index < _functionButtons.length) {
+                return _functionButtons[index];
+              }
+              int _contactIndex = index - _functionButtons.length;
+              Contact _contact = _contacts[_contactIndex];
+              bool _isGroupTitle = true;
+              if (_contactIndex >= 1 && _contact.nameIndex == _contacts[_contactIndex - 1].nameIndex) {
+                _isGroupTitle = false;
+              }
+              return _ContactItem(
+                avatar: _contact.avatar,
+                title: _contact.name,
+                groupTitle: _isGroupTitle ? _contact.nameIndex : null,
+              );
+            },
+            itemCount: _contacts.length + 4,
           ),
         ),
         Positioned(
@@ -117,8 +185,11 @@ class _ContactsPageState extends State<ContactsPage> {
           width: Constants.IndexBarWidth,
           top: 0.0,
           bottom: 0.0,
-          child: Column(
-            children: _letters,
+          child: Container(
+            color: _indexBarBg,
+            child: LayoutBuilder(
+              builder: _buildIndexBar,
+            )
           ),
         )
       ],
@@ -139,9 +210,19 @@ class _ContactItem extends StatelessWidget {
   final String title;
   final String groupTitle;
   final VoidCallback onPressed;
+  static const double MARGIN_VERTICAL = 10.0;
+  static const double GROUP_TITLE_HEIGHT = 32.0;
 
   bool get _isAvatarFromNet{
     return this.avatar.startsWith('http');
+  }
+
+  static double _height(bool hasGroupTitle) {
+    final _buttonHeight = MARGIN_VERTICAL * 2 + Constants.ContactAvatarSize + Constants.DividerWidth;
+    if (hasGroupTitle) {
+      return _buttonHeight + GROUP_TITLE_HEIGHT;
+    }
+    return _buttonHeight;
   }
 
   @override
@@ -164,8 +245,8 @@ class _ContactItem extends StatelessWidget {
 
     // 列表项主体部分
     Widget _button = Container(
-      padding: EdgeInsets.only(left: 8.0, right: 8.0, top: 10.0, bottom: 10.0),
-      margin: EdgeInsets.only(left: 8.0, right: 8.0),
+      padding: EdgeInsets.only(left: 8.0, right: 8.0, top: MARGIN_VERTICAL, bottom: MARGIN_VERTICAL),
+      height: _ContactItem._height(false),
       decoration: BoxDecoration(
         border: Border(
           bottom: BorderSide(
@@ -190,7 +271,8 @@ class _ContactItem extends StatelessWidget {
         children: <Widget>[
           Container(
             color: Color(AppColors.ContactGroupTitleBg),
-            padding: EdgeInsets.only(left: 18.0, top: 3.0, bottom: 3.0),
+            height: GROUP_TITLE_HEIGHT,
+            padding: EdgeInsets.only(left: 18.0),
             alignment: Alignment.centerLeft,
             child: Text(
               this.groupTitle,
